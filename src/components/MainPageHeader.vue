1dcba557f57c4503927f5c22a571f9de8e14a967
justin <jleoCoys@outlook.com>
2023-12-19 20:18:10 +0800

    done header

---
+++ src/components/MainPageHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="main"><h1>Tottenham Hotspur Players</h1></div>
+</template>
+
+<style scope>
+.main {
+  width: 100vw;
+  height: 80px;
+  background-color: #0b0e1e;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+}
+h1 {
+  margin: 0;
+}
+</style>
