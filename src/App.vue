3e870e4f08e02f7b6461e7475aaa9436278784fb
justin <jleoCoys@outlook.com>
2023-12-22 11:37:50 +0800

    added the content of the page

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <MainPageHeader></MainPageHeader>
+    <FootballPlayers></FootballPlayers>
   </div>
 </template>
 
 <script>
 import MainPageHeader from "./components/MainPageHeader.vue";
+import FootballPlayers from "./components/FootballPlayers.vue";
 
 export default {
   name: "App",
   components: {
     MainPageHeader,
+    FootballPlayers,
   },
 };
 </script>
---
+++ src/components/FootballPlayers.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="content">
+    <div
+      v-for="(player, index) in players"
+      :key="index"
+      class="player"
+      @click="player.show = !player.show"
+    >
+      <p class="player_name">{{ player.name }}</p>
+      <p>{{ player.location }}</p>
+      <p v-show="player.show" class="player_position">{{ player.position }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      players: [
+        {
+          name: "Heung-Min Son",
+          location: "Chuncheon, South Korea",
+          position: "Striker",
+          show: false,
+        },
+        {
+          name: "Richarlison",
+          location: "Nova Venecia, Brazil",
+          position: "Striker",
+          show: false,
+        },
+        {
+          name: "James Maddison",
+          location: "Coventry England",
+          position: "Playmaker",
+          show: false,
+        },
+        {
+          name: "Guglielmo Vicario",
+          location: "Udine, Italy",
+          position: "Goalkeeper",
+          show: false,
+        },
+        {
+          name: "Cristian Romero",
+          location: "Cordoba, Argentina",
+          position: "Defender",
+          show: false,
+        },
+        {
+          name: "Dejan Kulusevski",
+          location: "Stockholm, Sweden",
+          position: "Left Winger",
+          show: false,
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+.content {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  margin: 100px 50px;
+  gap: 20px;
+}
+
+.player {
+  height: 150px;
+  border: solid;
+  border-width: 1px;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  border-radius: 20px;
+  background: grey;
+  color: white;
+}
+
+.player_name {
+  font-size: 20px;
+}
+
+p {
+  margin-bottom: 10px;
+}
+
+.player_position {
+  font-weight: bolder;
+}
+</style>
