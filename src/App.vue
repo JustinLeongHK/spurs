d703876e67e904f55f46600d0ec3432838392933
justin <jleoCoys@outlook.com>
2023-12-22 11:52:53 +0800

    added footer

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
   <div id="app">
     <MainPageHeader></MainPageHeader>
     <FootballPlayers></FootballPlayers>
+    <page-footer></page-footer>
   </div>
 </template>
 
 <script>
 import MainPageHeader from "./components/MainPageHeader.vue";
 import FootballPlayers from "./components/FootballPlayers.vue";
+import PageFooter from "./components/PageFooter.vue";
 
 export default {
   name: "App",
   components: {
     MainPageHeader,
     FootballPlayers,
+    PageFooter,
   },
 };
 </script>
---
+++ src/components/PageFooter.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer>
+    <p>Copyright 2023 Tottenham Hotspur Players</p>
+  </footer>
+</template>
+
+<style>
+footer {
+  background: #0b0e1e;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 50px;
+  position: fixed;
+  bottom: 0px;
+  right: 0px;
+  left: 0px;
+  margin-top: 20px;
+}
+</style>
